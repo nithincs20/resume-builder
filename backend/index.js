59facadb82804d92f030efb658e28e4b2222d8cc
Snehatk2002 <sammu5285@gmail.com>
2025-03-06 20:13:03 +0530

    api

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,7 @@ app.use(express.json())
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello, API is running!' });
 });
+ 
 app.listen(8080, () => {
     console.log("server started")
-
-
 })
