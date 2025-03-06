d732bc8742454bea6ad9e30ce0879bb91988f14e
Sneha TK <161511364+Snehatk2002@users.noreply.github.com>
2025-03-06 17:53:43 +0530

    Api created (#7)
    closes https://github.com/nithincs20/resume-builder/issues/2

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express()
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello, API is running!' });
+});
+app.listen(8080, () => {
+    console.log("server started")
+
+
+})
