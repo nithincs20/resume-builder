891bafbfdd618542b1071be5d9fc8cf93eb00876
Nandana Krishna <nandana2564@gmail.com>
2025-03-10 02:05:45 +0530

    API for uploaded files

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -39,3 +39,31 @@ function openModal(name, imageSrc) {
 function closeModal() {
     document.getElementById("resumeModal").style.display = "none";
 }
+
+//Resume selcted
+function updateLabel(inputElement) {
+    let labelSpan = inputElement.nextElementSibling; 
+    if (inputElement.files.length > 0) {
+        labelSpan.textContent = "Resume Selected"; 
+    } else {
+        labelSpan.textContent = "Select Resume"; 
+    }
+}
+
+//Get files
+async function uploadFile() {
+   
+    const fileInput = document.getElementById("fileInput");
+    const formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+  
+   //Files in :http://localhost:5000/files
+
+    const res = await fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: formData,
+    });
+  
+    const data = await res.json();
+    console.log("File URL:", data.fileUrl);
+  }
