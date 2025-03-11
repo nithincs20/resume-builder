5f93504d1585e8d95a2951121b5125226ac38610
Nandana Krishna <nandana2564@gmail.com>
2025-03-11 23:37:51 +0530

    API for upload

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 
 
 const templates = [
@@ -25,3 +27,37 @@ app.get('/templates', (req, res) => {
 app.listen(8080, () => {
     console.log("Server started ");
 });
+
+app.use(cors());
+app.use(express.json());
+
+//Storage 
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use("/uploads", express.static("uploads"));
+
+//Files uploaded
+app.post("/upload", upload.single("file"), (req, res) => {
+  if (!req.file) return res.status(400).send("No file uploaded.");
+  res.json({ message: "File uploaded!", fileUrl: `/uploads/${req.file.filename}` });
+});
+
+app.get("/files", (req, res) => {
+  const fs = require("fs");
+  const files = fs.readdirSync("uploads/").map((file) => ({
+    name: file,
+    url: `/uploads/${file}`,
+  }));
+  res.json(files);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
