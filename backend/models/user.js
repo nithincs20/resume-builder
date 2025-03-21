16a4248cbf3a6629bc2ac8a6ed07d0575c3804cb
Snehatk2002 <sammu5285@gmail.com>
2025-03-21 10:12:02 +0530

    admin login created

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,8 @@ const userSchema=mongoose.Schema(
     {
         username:{type:String,required:true},
         email:{type:String,required:true},
-        password:{type:String,required:true}
+        password:{type:String,required:true},
+        usertype:{type:String,required:true}
         
 
     });
